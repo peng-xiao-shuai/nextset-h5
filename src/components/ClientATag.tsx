3a3d951ab839faa15e2abe963807926775704a18
peng-xiao-shuai <1612565136@qq.com>
2025-07-22 21:35:53 +0800

    feat(H5): 新增 ClientATag 组件，点击触发app函数打开邮箱发现邮件

---
+++ src/components/ClientATag.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { appInfo } from '@/config/ConfigData';
+import Link from 'next/link';
+
+export function ClientOnlyLink() {
+  function handleClick() {
+    const isPhone = /Phone/i.test(navigator.userAgent);
+
+    if (isPhone) {
+      window.webProxy.openMailbox();
+    } else {
+      window.open(`mailto:${appInfo.email}`, '_blank');
+    }
+  }
+
+  return (
+    <Link
+      href="javascript:void(0)"
+      onClick={handleClick}
+      className="font-bold hover:underline"
+    >
+      {appInfo.email}
+    </Link>
+  );
+}
